dc4e05c43b47e779e8291aeaa9f14f0091af4459
kpryzk <kepryzk@gmail.com>
2017-09-07 15:37:46 -0700

    child route and one working test

--- lab-katherine/model/toy.js
+++ lab-katherine/model/toy.js
@@ -15,14 +15,14 @@ Toy.pre('save', function(next) {
   debug('#pre-save Toy')
 
   Child.findById(this.child)
-  .then(child => {
-    let toyIdSet = new Set(child.toys)
-    toyIdSet.add(this._id) // only going to to add if the _id isn't already present in the set
-    child.toys = Array.from(toyIdSet)
-    return child.save()
-  })
-  .then(next)
-  .catch(() => next(new Error('validation failed to create toy because child does not exist')))
+    .then(child => {
+      let toyIdSet = new Set(child.toys)
+      toyIdSet.add(this._id) // only going to to add if the _id isn't already present in the set
+      child.toys = Array.from(toyIdSet)
+      return child.save()
+    })
+    .then(next)
+    .catch(() => next(new Error('validation failed to create toy because child does not exist')))
 })
 
 Toy.post('remove', function(doc, next) {
--- lab-katherine/route/route-child.js
+++ lab-katherine/route/route-child.js
@@ -11,7 +11,42 @@ module.exports = function(router) {
     debug('/api/child POST')
 
     return new Child(req.body).save()
-    .then(child => res.status(201).json(child))
-    .catch(err => errorHandler(err, req, res))
+      .then(child => res.status(201).json(child))
+      .catch(err => errorHandler(err, req, res))
+  })
+
+  //GET CHILD AND TOYS
+  router.get('/api/child/:_id', (req, res) => {
+    debug('/api/child/:_id GET')
+
+    return Child.findById(req.params._id)
+      .populate('toys')
+      .then(child => res.json(child))
+      .catch(err => errorHandler(err, req, res))
+  })
+
+  //GET ALL CHILDREN
+  router.get('/api/child', (req, res) => {
+    debug('/api/child GET')
+
+    return Child.find()
+      .then(children => res.json(children.map(child => child._id)))
+      .catch(err => errorHandler(err, req, res))
+  })
+
+  router.put('/api/child/:_id', (req, res) => {
+    debug('/api/child PUT')
+
+    return Child.findByIdAndUpdate(req.params._id, req.body, { upsert:true, runValidators:true })
+      .then(() => res.sendStatus(204))
+      .catch(err => errorHandler(err, req, res))
+  })
+
+  router.delete('/api/child/:_id', (req, res) => {
+    debug('/api/child DELETE')
+
+    return Child.findByIdAndRemove(req.params._id)
+      .then(() => res.sendStatus(204))
+      .catch(err => errorHandler(err, req, res))
   })
 }
