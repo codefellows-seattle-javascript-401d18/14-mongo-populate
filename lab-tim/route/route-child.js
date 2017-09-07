6bed571fed952f9264c27910808755398987b7c1
Tim Turner <timmyturner@me.com>
2017-09-07 14:16:20 -0700

    working on child methods

--- lab-tim/route/route-child.js
+++ lab-tim/route/route-child.js
@@ -14,4 +14,36 @@ module.exports = function(router) {
     .then(child => res.status(201).json(child))
     .catch(err => errorHandler(err, req, res));
   });
+
+  router.get('/api/toy/:_id', (req, res) => {
+    debug('/api/toy/:_id GET');
+
+    return Child.findById(req.params._id)
+      .then(toy => res.status(200).json(toy))
+      .catch(err => errorHandler(err, req, res));
+  });
+
+  router.get('/api/toy', (req, res) => {
+    debug('/api/toy GET all');
+
+    return Child.find()
+      .then(toys => res.json(toys.map(toy => toy._id)))
+      .catch(err => errorHandler(err, req, res));
+  });
+
+  router.put('/api/toy/:_id', (req, res) => {
+    debug('/api/toy PUT');
+
+    return Child.findByIdAndUpdate(req.params._id, req.body, { upsert:true, runValidators:true })
+      .then(toy => res.status(204).json(toy))
+      .catch(err => errorHandler(err, req, res));
+  });
+
+  router.delete('/api/toy/:_id', (req, res) => {
+    debug('/api/toy DELETE');
+
+    return Child.findByIdAndRemove(req.params._id)
+      .then(() => res.sendStatus(204))
+      .catch(err => errorHandler(err, req, res));
+  });
 };
