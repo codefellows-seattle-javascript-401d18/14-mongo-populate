bd2524b1ac223d9e3f1d125f0807c9f6ee3f7a25
Shaun Wahle <shaun.wahle@gmail.com>
2017-09-07 15:02:25 -0700

    Seem to be having server issues

--- shaun-lab13/lib/server.js
+++ shaun-lab13/lib/server.js
@@ -1,6 +1,7 @@
+
 'use strict';
 
-//const debug = require('debug')('http:server');
+// const debug = require('debug')('http:server');
 require('dotenv').config();
 
 // express setup
@@ -10,26 +11,18 @@ const app = express();
 
 // mongoose setup
 const mongoose = require('mongoose');
-const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/toy-dev';
 mongoose.Promise = require('bluebird');
-mongoose.connect(MONGODB_URI, {useMongoClient: true});
-
-// middleware
-const bodyParser = require('body-parser').json();
-const cors = require('cors');
-const errorMiddleware = require('./error-middleware');
+mongoose.connect(process.env.MONGODB_URI, {useMongoClient: true});
 
 // routes (middleware)
 require('../route/route-toy')(router);
-require('./route/route-child')(router);
+require('../route/route-child')(router);
 // require('./route/route-family')(router)
 
 // mount middleware
-app.use(bodyParser);
-app.use(cors());
+app.use(require('body-parser').json());
+app.use(require('cors')());
 app.use(router);
-// this should always be last to catch any errors within the callback chain
-app.use(errorMiddleware);
 
 app.all('/*', (req, res) => res.sendStatus(404));
 
